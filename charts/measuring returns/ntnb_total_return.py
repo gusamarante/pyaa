363f821d4fa5c432ff9f105bf0ec95d6c617191e
gamarante <gusamarante@gmail.com>
2023-11-03 10:47:09 -0200

    Starting the NTNB Total Return Chart

---
+++ charts/measuring returns/ntnb_total_return.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+filepath = r'C:\Users\gamarante\PycharmProjects\pyaa\trackers\output data\trackers_ntnb.xlsx'  # Work
+df = pd.read_excel(filepath, index_col=0)
+
+
+# TODO make the charts

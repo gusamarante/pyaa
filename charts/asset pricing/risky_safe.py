8a4ed8f208066a3997661d2a20da0ce40475baa5
gamarante <gusamarante@gmail.com>
2024-02-20 13:44:44 -0300

    finished the chart for the AP course

--- charts/asset pricing/risky_safe.py
+++ charts/asset pricing/risky_safe.py
@@ -4,24 +4,28 @@ Grabs 3 brazilian assets and builds the min variance frontier.
 
 import pandas as pd
 from utils import Performance, SGS
+from allocation import MeanVar
 
 # =====================
 # ===== Read Data =====
 # =====================
 # IVVB
-filepath = '/Users/gustavoamarante/Library/CloudStorage/Dropbox/Personal Portfolio/data/ETFs.xlsx'
+# filepath = '/Users/gustavoamarante/Library/CloudStorage/Dropbox/Personal Portfolio/data/ETFs.xlsx'  # Mac
+filepath = r"C:\Users\gamarante\Dropbox\Personal Portfolio\data\ETFs.xlsx"  # Work
 ivvb = pd.read_excel(filepath, index_col=0, na_values=['#N/A N/A'], sheet_name='values')
 ivvb = ivvb['IVVB11 BZ Equity'].rename('IVVB')
 ivvb = ivvb.dropna()
 
 # BDIV
-filepath = '/Users/gustavoamarante/Library/CloudStorage/Dropbox/Personal Portfolio/data/FIPs.xlsx'
+# filepath = '/Users/gustavoamarante/Library/CloudStorage/Dropbox/Personal Portfolio/data/FIPs.xlsx'  # Mac
+filepath = r"C:\Users\gamarante\Dropbox\Personal Portfolio\data\FIPs.xlsx"  # Work
 bdiv = pd.read_excel(filepath, index_col=0, sheet_name='Trackers')
 bdiv = bdiv['BDIV']
 bdiv = bdiv.dropna()
 
 # NTNB
-filepath = '/Users/gustavoamarante/PycharmProjects/pyaa/trackers/output data/trackers_ntnb.xlsx'
+# filepath = '/Users/gustavoamarante/PycharmProjects/pyaa/trackers/output data/trackers_ntnb.xlsx'  # Mac
+filepath = r"C:\Users\gamarante\PycharmProjects\pyaa\trackers\output data\trackers_ntnb.xlsx"  # Work
 ntnb = pd.read_excel(filepath, index_col=0)
 ntnb = ntnb['NTNB 10y']
 
@@ -40,11 +44,24 @@ eri = eri.sub(cdi, axis=0)
 eri = eri.dropna(how='all')
 eri = (1 + eri).cumprod()
 
-# =========================================
-# ===== Correlation of Excess Returns =====
-# =========================================
+# ==========================
+# ===== Excess Returns =====
+# ==========================
+# Performance
+perf_tri = Performance(tri, skip_dd=True)
+print(perf_tri.table)
+
+perf_eri = Performance(eri, skip_dd=True)
+print(perf_eri.table)
+
+# Correlation
 corr = eri.resample('M').last().pct_change(1).corr()
 print(corr)
 
-perf = Performance(eri, skip_dd=True)
-print(perf.table)
+# Min Variance Parameters
+mu = perf_tri.returns_ann
+cov = eri.resample('M').last().pct_change(1).cov() * 12
+rf = (1 + cdi.iloc[-1])**252 - 1
+
+mv = MeanVar(mu, cov, rf=rf, risk_aversion=100)
+mv.plot(mvfnoss=False)

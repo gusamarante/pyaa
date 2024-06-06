282f1cedbd80bc43910b6247d1e6343d9c7fa39b
gamarante <gustavo.amarante@bwgi.com.br>
2024-06-06 11:54:56 -0300

    Chart of cumulative performance for brazilian HFs

--- courses/asset allocation/managers.py
+++ courses/asset allocation/managers.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from data.data_api import SGS
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
 
 
 # ================
@@ -37,6 +39,37 @@ df_trackers = df_trackers / df_trackers.bfill().iloc[0]
 df_trackers = df_trackers.dropna(how='all')
 
 df_trackers.to_clipboard()
+
+
+# All cumulative ERI
+df_cum = []
+for fund in df_trackers.columns:
+    df_cum.append(df_trackers[fund].dropna().reset_index(drop=True))
+
+df_cum = pd.concat(df_cum, axis=1)
+df_cum.index = df_cum.index / 252
+
+
+# ===========================================
+# ===== Chart - Cumulative ERI Together =====
+# ===========================================
+fig = plt.figure(figsize=(7 * (16 / 7.3), 7))
+ax = plt.subplot2grid((1, 1), (0, 0))
+ax.set_title("Brazilian Hedge Funds - Excess Return Index")
+ax.plot(df_cum)
+ax.axhline(1, color='black', lw=0.5)
+ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
+ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5, which="both")
+ax.set_yscale('log')
+ax.set_ylabel("Index (log-scale)")
+ax.set_xlabel("Years since start of series")
+ax.get_yaxis().set_major_formatter(ScalarFormatter())
+
+
+plt.tight_layout()
+
+plt.show()
+plt.close()
 
 # TODO performance from the start
 # TODO Correlation Linkage

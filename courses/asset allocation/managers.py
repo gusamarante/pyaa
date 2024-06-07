612478b2c0dbb70bd94dfae2023bbfbcb92937f5
gamarante <gustavo.amarante@bwgi.com.br>
2024-06-07 16:51:56 -0300

    finished managers analysis

--- courses/asset allocation/managers.py
+++ courses/asset allocation/managers.py
@@ -42,16 +42,16 @@ for fund in df.columns:
     df_xr.append(ret.rename(fund))
 
 df_xr = pd.concat(df_xr, axis=1)
-df_trackers = (1 + df_xr).cumprod()
-df_trackers = df_trackers / df_trackers.bfill().iloc[0]
-df_trackers = df_trackers.dropna(how='all')
+df_funds = (1 + df_xr).cumprod()
+df_funds = df_funds / df_funds.bfill().iloc[0]
+df_funds = df_funds.dropna(how='all')
 
-df_trackers.to_clipboard()
+df_funds.to_clipboard()
 
 # All cumulative ERI
 df_cum = []
-for fund in df_trackers.columns:
-    df_cum.append(df_trackers[fund].dropna().reset_index(drop=True))
+for fund in df_funds.columns:
+    df_cum.append(df_funds[fund].dropna().reset_index(drop=True))
 
 df_cum = pd.concat(df_cum, axis=1)
 df_cum.index = df_cum.index / 252
@@ -86,7 +86,7 @@ df_assets = pd.concat([ntnf, ntnb, ivvb, ida], axis=1).dropna(how='all')
 backtest = pd.Series(name='Backtest')
 # df_vols = df_assets.pct_change(21).rolling(252).std() * np.sqrt(12)  # Traditional
 df_vols = df_assets.pct_change(21).ewm(com=252).std() * np.sqrt(12)  # EWM
-notional_start = 100
+notional_start = 1
 
 # We compute weights every day beacause it is easy, but we are only going to use them on rebalance dates.
 inv_vol_weights = (1/df_vols).div((1/df_vols).sum(axis=1), axis=0)  # Traditional
@@ -127,15 +127,20 @@ perf_port = Performance(backtest.to_frame('Simple'), skip_dd=True)
 print(perf_port.table)
 
 
-# =======================
-# ===== Performance =====
-# =======================
-perf = Performance(df_trackers, skip_dd=True)
+# ==============================
+# ===== Performance of HFs =====
+# ==============================
+perf = Performance(df_funds, skip_dd=True)
+correls = df_funds.pct_change(21).corrwith(backtest.pct_change(21)).sort_values(ascending=False)
+
+hr = df_funds.resample('M').last().pct_change(1).sub(backtest.resample('M').last().pct_change(1), axis=0)
+hr = (hr > 0).where(~hr.isna())
+# (hr.sum(axis=1).rolling(12).sum() / hr.count(axis=1).rolling(12).sum()).plot()
 
 
-# ===========================================
-# ===== Chart - Cumulative ERI Together =====
-# ===========================================
+# =================================================
+# ===== Chart - Cumulative ERI of Hedge Funds =====
+# =================================================
 fig = plt.figure(figsize=(7 * (16 / 7.3), 7))
 ax = plt.subplot2grid((1, 1), (0, 0))
 ax.set_title("Brazilian Hedge Funds - Excess Return Index")
@@ -154,15 +159,39 @@ plt.show()
 plt.close()
 
 
+# =====================================================
+# ===== Chart - Cumulative ERI of Simple Strategy =====
+# =====================================================
+fig = plt.figure(figsize=(7 * (16 / 7.3), 7))
+ax = plt.subplot2grid((1, 1), (0, 0))
+ax.set_title("Simple Strategy VS Brazilian Hedge Funds - Excess Return Index")
+ax.plot(df_cum, color='lightgrey', alpha=0.7, label=None)
+
+bt2plot = backtest.dropna().reset_index(drop=True)
+bt2plot.index = bt2plot.index / 252
+ax.plot(bt2plot, color="tab:blue", lw=2, label="Simple Strategy Backtest")
+ax.axhline(1, color='black', lw=0.5)
+ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
+ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5, which="both")
+ax.set_yscale('log')
+ax.set_ylabel("Index (log-scale)")
+ax.set_xlabel("Years since start of series")
+ax.get_yaxis().set_major_formatter(ScalarFormatter())
+ax.legend(frameon=True, loc='best')
+
+plt.tight_layout()
+
+plt.show()
+plt.close()
+
+
 # ============================================
 # ===== Chart - Correlation + Dendrogram =====
 # ============================================
-cov = df_trackers.dropna().pct_change(21).cov()
+cov = df_funds.dropna().pct_change(21).cov()
 hrp = HRP(cov)
 hrp.plot_dendrogram()
 hrp.plot_corr_matrix()
 
-# TODO Incluir trajetória no gráfico com HFs opacos
-# TODO Calcular correlações com os HFs
 # TODO percentual de HFs de quem o portfolio ganha
 # TODO Percentual de anos que um HF ganha do Fundo

343b1dbb9c015dfe76047ba2704d7b111e4aa5d2
gustavoamarante <gusamarante@gmail.com>
2024-07-02 06:40:12 -0300

    DI all fixed maturities

--- trackers/di1_fixed_maturities.py
+++ trackers/di1_fixed_maturities.py
@@ -3,11 +3,11 @@ from tqdm import tqdm
 import pandas as pd
 import numpy as np
 from time import time
+from data.data_api import SGS
 
 tic = time()
 
 # User defined parameters
-desired_maturities = [252 * t for t in range(1, 11)]  # in DUs
 last_year = 2024
 start_date = "2008-01-01"
 
@@ -23,9 +23,13 @@ data['maturity_date'] = pd.to_datetime(data['maturity_date'])
 data = data.drop('Unnamed: 0', axis=1)
 
 
+# Grab CDI for columns 0
+cdi = SGS().fetch({12: 'CDI'})
+cdi_ann = (1 + cdi['CDI'] / 100) ** 252 - 1
+
 # ===== Interpolate Flat-Forward =====
 curve = data.pivot(index='reference_date', columns='du', values='rate')
-curve = curve.drop(0, axis=1)
+curve[0] = cdi_ann
 curve = curve[curve.index >= start_date]
 
 # generate the log-discount
@@ -38,12 +42,12 @@ curve = curve.interpolate(method='index', axis=1, limit_area='inside')
 curve = (1 / np.exp(curve)) ** (252 / curve.columns) - 1
 
 # Keep desired columns and drop NAs
-curve = curve[desired_maturities]
-curve.columns = [f"{int(mat/252)}y" for mat in curve.columns]
-curve = curve.dropna(how='any', axis=1)
+curve = curve.drop(0, axis=1)
+curve.columns = [int(mat) for mat in curve.columns]
+curve = curve.dropna(how='all', axis=1)
+curve = curve.dropna(how='all', axis=0)
 curve = curve.dropna()
 
-print(curve)
 # ===== Save rates =====
 filename = r'output data/di fixed maturities.xlsx'
 with pd.ExcelWriter(filename) as writer:

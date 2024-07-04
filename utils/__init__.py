8af5c6257c9ed666627f2a4946e59246370d88ff
gustavoamarante <gusamarante@gmail.com>
2024-07-03 22:06:37 -0300

    Minor changes

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,14 +1,11 @@
 from utils.performance import Performance, compute_eri
-from data.data_api import SGS, FRED
 from utils.stats import cov2corr, corr2cov
 from utils.paths import AA_LECTURE
 
 __all__ = [
     "AA_LECTURE",
-    'FRED',
-    'Performance',
-    'SGS',
+    'compute_eri',
     'corr2cov',
     'cov2corr',
-    'compute_eri',
+    'Performance',
 ]

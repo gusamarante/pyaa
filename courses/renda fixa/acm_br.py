0064de6dc546fe7fa175b9b17ad4d05b2b8ae619
Gustavo Amarante <gusamarante@gmail.com>
2024-07-07 16:52:12 -0300

    Touch up

--- courses/renda fixa/acm_br.py
+++ courses/renda fixa/acm_br.py
@@ -7,30 +7,12 @@ di = curve_di()
 di = di.loc[:, :'120m'].dropna()
 di = di[di.index >= "2008-01-01"]
 
-
-# Convert yields to log-yields
-# TODO should this go inside the class?
-# log_di = np.log(1 + di)
-# di = di.resample('M').last()
-
 acm = NominalACM(
     curve=di,
 )
 
-mat = "6m"
-di[mat].plot()
-acm.miy[mat].plot()
-acm.rny[mat].plot()
-plt.show()
-
 mat = "60m"
-di[mat].plot()
-acm.miy[mat].plot()
-acm.rny[mat].plot()
-plt.show()
-
-mat = "120m"
-di[mat].plot()
-acm.miy[mat].plot()
-acm.rny[mat].plot()
+di[mat].rename('Observed').plot(title="5y DI", legend=True)
+acm.miy[mat].rename('Fitted').plot(legend=True)
+acm.rny[mat].rename('Risk-Neutral').plot(legend=True)
 plt.show()

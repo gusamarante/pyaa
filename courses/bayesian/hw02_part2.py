1390f4ef32f8308334ff46ca8fb404a9cf89cc10
Gustavo Amarante <gusamarante@gmail.com>
2024-09-07 15:02:18 -0300

    part 2 item 3

--- courses/bayesian/hw02_part2.py
+++ courses/bayesian/hw02_part2.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-m = 5000000  # Samples from candidate
-n = 1000000  # Weighted samples from set
+m = 5000  # Samples from candidate
+n = 1000 # Weighted samples from set
 
 inc = 0.1
 theta_grid = np.arange(
@@ -19,47 +19,47 @@ laplace_pdf = laplace.pdf(theta_grid, loc=0, scale=1)
 
 
 # Chart comparing candidate and target densities
-# size = 4
-# fig = plt.figure(figsize=(size * (16 / 7.3), size))
-#
-# ax = plt.subplot2grid((3, 2), (0, 0), rowspan=2)
-# ax.plot(theta_grid, t_pdf, label=f"t(3)", lw=2)
-# ax.plot(theta_grid, norm_pdf, label=f"N(0,1)", lw=2)
-# ax.set_ylim(0, None)
-# ax.set_ylabel(r"Probability Density")
-# ax.set_xlabel(r"$\theta$")
-# ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
-# ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
-# ax.legend(frameon=True, loc="best")
-#
-# ax = plt.subplot2grid((3, 2), (2, 0))
-# ax.plot(theta_grid, norm_pdf / t_pdf, label=f"Weight / Density Ratio", lw=2)
-# ax.set_ylabel(r"Density Ratio")
-# ax.set_xlabel(r"$\theta$")
-# ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
-# ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
-#
-# ax = plt.subplot2grid((3, 2), (0, 1), rowspan=2)
-# ax.plot(theta_grid, t_pdf, label=f"t(3)", lw=2)
-# ax.plot(theta_grid, laplace_pdf, label=f"Laplace(0,1)", lw=2)
-# ax.set_ylim(0, None)
-# ax.set_ylabel(r"Probability Density")
-# ax.set_xlabel(r"$\theta$")
-# ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
-# ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
-# ax.legend(frameon=True, loc="best")
-#
-# ax = plt.subplot2grid((3, 2), (2, 1))
-# ax.plot(theta_grid, laplace_pdf / t_pdf, label=f"Weight / Density Ratio", lw=2)
-# ax.set_ylabel(r"Density Ratio")
-# ax.set_xlabel(r"$\theta$")
-# ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
-# ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
-#
-# plt.tight_layout()
-# plt.savefig("/Users/gustavoamarante/Library/CloudStorage/Dropbox/Aulas/Doutorado - Bayesiana/HW02/part 2 candidate target.pdf")
-# plt.show()
-# plt.close()
+size = 4
+fig = plt.figure(figsize=(size * (16 / 7.3), size))
+
+ax = plt.subplot2grid((3, 2), (0, 0), rowspan=2)
+ax.plot(theta_grid, t_pdf, label=f"t(3)", lw=2)
+ax.plot(theta_grid, norm_pdf, label=f"N(0,1)", lw=2)
+ax.set_ylim(0, None)
+ax.set_ylabel(r"Probability Density")
+ax.set_xlabel(r"$\theta$")
+ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
+ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
+ax.legend(frameon=True, loc="best")
+
+ax = plt.subplot2grid((3, 2), (2, 0))
+ax.plot(theta_grid, norm_pdf / t_pdf, label=f"Weight / Density Ratio", lw=2)
+ax.set_ylabel(r"Density Ratio")
+ax.set_xlabel(r"$\theta$")
+ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
+ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
+
+ax = plt.subplot2grid((3, 2), (0, 1), rowspan=2)
+ax.plot(theta_grid, t_pdf, label=f"t(3)", lw=2)
+ax.plot(theta_grid, laplace_pdf, label=f"Laplace(0,1)", lw=2)
+ax.set_ylim(0, None)
+ax.set_ylabel(r"Probability Density")
+ax.set_xlabel(r"$\theta$")
+ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
+ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
+ax.legend(frameon=True, loc="best")
+
+ax = plt.subplot2grid((3, 2), (2, 1))
+ax.plot(theta_grid, laplace_pdf / t_pdf, label=f"Weight / Density Ratio", lw=2)
+ax.set_ylabel(r"Density Ratio")
+ax.set_xlabel(r"$\theta$")
+ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
+ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
+
+plt.tight_layout()
+plt.savefig("/Users/gustavoamarante/Library/CloudStorage/Dropbox/Aulas/Doutorado - Bayesiana/HW02/part 2 candidate target.pdf")
+plt.show()
+plt.close()
 
 
 # =============================
@@ -139,5 +139,5 @@ ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
 ax.legend(loc='best', frameon=True)
 
 plt.tight_layout()
-# plt.savefig("/Users/gustavoamarante/Library/CloudStorage/Dropbox/Aulas/Doutorado - Bayesiana/HW02/part 2 item iii.pdf")
+plt.savefig("/Users/gamarante/Library/CloudStorage/Dropbox/Aulas/Doutorado - Bayesiana/HW02/part 2 item iii.pdf")
 plt.show()

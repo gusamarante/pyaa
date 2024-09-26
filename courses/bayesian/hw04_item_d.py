b53f64621ab6d2b1b9d8c2a45957811791073572
Gustavo Amarante <gusamarante@gmail.com>
2024-09-25 23:57:15 -0400

    Progress on the last question

--- courses/bayesian/hw04_item_d.py
+++ courses/bayesian/hw04_item_d.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import norm
+from scipy.stats import norm, invgamma
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
@@ -37,59 +37,97 @@ obs = simul_ar1(
 )
 
 
+# ===========================
 # ===== Gibbs samplings =====
-gibbs = pd.DataFrame(
-    columns=[k + 1 for k in range(n)],
-)
-gibbs.loc[0] = obs
-
-a1 = 0
-R1 = 9
-R2 = R1 ** 2
-
-for ng in tqdm(range(1, n_tot + 1)):
-    for nt in range(1, n + 1):
-
-        if nt == 1:  # Start of the series
-            T1 = sig2 * R2 * alpha * beta - tau2 * R2 * obs[0] - sig2 * R2 * beta * gibbs.loc[ng-1, 2] - sig2 * tau2 * a1
-            T2 = tau2 * R2 + sig2 * R2 * (beta**2) + sig2 * tau2
-            Tmu = - T1 / T2
-            Tsig = np.sqrt(sig2 * tau2 * R2 / T2)
-            gibbs.loc[ng, nt] = norm.rvs(loc=Tmu, scale=Tsig)
-
-        elif nt == n:  # End of the series
-            Q1 = - tau2 * obs[-1] - sig2 * alpha - sig2 * beta * gibbs.loc[ng, nt - 1]
-            Q2 = sig2 + tau2
-            Qmu = - Q1 / Q2
-            Qsig = np.sqrt(sig2 * tau2 / Q2)
-            gibbs.loc[ng, nt] = norm.rvs(loc=Qmu, scale=Qsig)
-
-        else:  # Somewhere in the middle
-            M1 = sig2 * alpha * beta - tau2 * obs[nt - 1] - sig2 * beta * gibbs.loc[ng - 1, nt + 1] - sig2 * alpha - sig2 * beta * gibbs.loc[ng, nt - 1]
-            M2 = tau2 + sig2 * (beta ** 2) + sig2
-            Mmu = - M1 / M2
-            Msig = np.sqrt(sig2 * tau2 / M2)
-            gibbs.loc[ng, nt] = norm.rvs(loc=Mmu, scale=Msig)
-
-cis = gibbs.quantile(q=[0.025, 0.5, 0.975])
-cis = cis.iloc[-n_gibbs:]
+# ===========================
+# gibbs = pd.DataFrame(
+#     columns=[k + 1 for k in range(n)],
+# )
+# gibbs.loc[0] = obs
+#
+# a1 = 0
+# R1 = 9
+# R2 = R1 ** 2
+#
+# for ng in tqdm(range(1, n_tot + 1)):
+#     for nt in range(1, n + 1):
+#
+#         if nt == 1:  # Start of the series
+#             T1 = sig2 * R2 * alpha * beta - tau2 * R2 * obs[0] - sig2 * R2 * beta * gibbs.loc[ng-1, 2] - sig2 * tau2 * a1
+#             T2 = tau2 * R2 + sig2 * R2 * (beta**2) + sig2 * tau2
+#             Tmu = - T1 / T2
+#             Tsig = np.sqrt(sig2 * tau2 * R2 / T2)
+#             gibbs.loc[ng, nt] = norm.rvs(loc=Tmu, scale=Tsig)
+#
+#         elif nt == n:  # End of the series
+#             Q1 = - tau2 * obs[-1] - sig2 * alpha - sig2 * beta * gibbs.loc[ng, nt - 1]
+#             Q2 = sig2 + tau2
+#             Qmu = - Q1 / Q2
+#             Qsig = np.sqrt(sig2 * tau2 / Q2)
+#             gibbs.loc[ng, nt] = norm.rvs(loc=Qmu, scale=Qsig)
+#
+#         else:  # Somewhere in the middle
+#             M1 = sig2 * alpha * beta - tau2 * obs[nt - 1] - sig2 * beta * gibbs.loc[ng - 1, nt + 1] - sig2 * alpha - sig2 * beta * gibbs.loc[ng, nt - 1]
+#             M2 = tau2 + sig2 * (beta ** 2) + sig2
+#             Mmu = - M1 / M2
+#             Msig = np.sqrt(sig2 * tau2 / M2)
+#             gibbs.loc[ng, nt] = norm.rvs(loc=Mmu, scale=Msig)
+#
+# cis = gibbs.quantile(q=[0.025, 0.5, 0.975])
+# cis = cis.iloc[-n_gibbs:]
 
 
 # =================
 # ===== CHART =====
 # =================
-size = 5
-fig = plt.figure(figsize=(size * (16 / 9), size))
-ax = plt.subplot2grid((1, 1), (0, 0))
-ax.plot(cis.columns, obs, label=rf"Observed $y_t$", lw=2)
-ax.plot(cis.loc[0.5], label=rf"Median", lw=2)
-ax.fill_between(cis.columns, cis.loc[0.025], cis.loc[0.975], label="95% CI", lw=0, color='grey', alpha=0.3)
-ax.set_xlabel(r"$t$")
-ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
-ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
-ax.legend(frameon=True, loc="best")
+# size = 5
+# fig = plt.figure(figsize=(size * (16 / 9), size))
+# ax = plt.subplot2grid((1, 1), (0, 0))
+# ax.plot(cis.columns, obs, label=rf"Observed $y_t$", lw=2)
+# ax.plot(cis.loc[0.5], label=rf"Median", lw=2)
+# ax.fill_between(cis.columns, cis.loc[0.025], cis.loc[0.975], label="95% CI", lw=0, color='grey', alpha=0.3)
+# ax.set_xlabel(r"$t$")
+# ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
+# ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
+# ax.legend(frameon=True, loc="best")
+#
+# plt.tight_layout()
+# plt.savefig("/Users/gamarante/Library/CloudStorage/Dropbox/Aulas/Doutorado - Bayesiana/HW04/ar1 gibbs.pdf")
+# plt.show()
+# plt.close()
 
-plt.tight_layout()
-plt.savefig("/Users/gamarante/Library/CloudStorage/Dropbox/Aulas/Doutorado - Bayesiana/HW04/ar1 gibbs.pdf")
-plt.show()
-plt.close()
+
+# ======================================
+# ===== Gibbs samplings with Gamma =====
+# ======================================
+mu_alpha = 0
+mu_beta = 0
+delta = 2
+
+a_sigma = 3  # TODO comment mena and stdev of 1
+b_sigma = 2
+
+a_tau = 3
+b_tau = 2
+
+
+cols = [k + 1 for k in range(n)]
+cols.append('alpha')
+cols.append('beta')
+cols.append('sigma2')
+cols.append('tau2')
+
+gibbs = pd.DataFrame(
+    columns=cols,
+)
+
+gibbs.loc[0, 1:n] = obs
+gibbs.loc[0, 'alpha'] = 0
+gibbs.loc[0, 'beta'] = 0
+gibbs.loc[0, 'sigma2'] = invgamma.mean(a=a_sigma, scale=b_sigma)
+gibbs.loc[0, 'tau2'] = invgamma.mean(a=a_tau, scale=b_tau)
+
+# TODO montar o gibbs sampling com todas as variáveis
+
+
+print(gibbs)

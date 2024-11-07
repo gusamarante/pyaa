6f984167e87ddff829c1512fb955cf372830cec2
gamarante <gustavo.amarante@bwgi.com.br>
2024-11-07 14:41:27 -0300

    Adding documentation to BFM

--- models/bfm.py
+++ models/bfm.py
@@ -7,12 +7,21 @@ from numpy.linalg import inv, svd
 from scipy.stats import invwishart, multivariate_normal
 from time import time
 
-# TODO add frequentist FM to charts
-
 
 class FM:
 
     def __init__(self, assets, factors):
+        """
+        Very simple implementation of the Fama-MacBeth regression using OLS.
+
+        Parameters
+        ----------
+        assets : pandas.DataFrame
+            returns of the test assets
+
+        factors : pandas.DataFrame
+            returns of the factor portfolios
+        """
 
         self.beta = pd.DataFrame(
             data=(inv(factors.T @ factors) @ factors.T @ assets).values,
@@ -29,17 +38,34 @@ class FM:
 
 
 class BFM:
-    # TODO Documentation
+    """
+    This class works as both the baseline implementation of the Bayesian
+    Fama-MacBeth and the parent class for other variations, which follow the
+    same steps, but with different computations.
+    """
 
     def __init__(self, assets, factors, n_draws=1000):
-        # TODO Documentation
+        """
+        Implementation of the BFM-OLS
+
+        Parameters
+        ----------
+        assets : pandas.DataFrame
+            returns of the test assets
+
+        factors : pandas.DataFrame
+            returns of the factor portfolios
+
+        n_draws : int
+            number of draws from the posterior
+        """
         self.assets = assets
         self.factors = factors
         self.n_draws = n_draws
         self.y = pd.concat([assets, factors], axis=1).dropna()
 
-        self.t = self.y.shape[0]
-        self.n = assets.shape[1]  # Number of assets
+        self.t = self.y.shape[0]  # Sample size of the timeseries dimension
+        self.n = assets.shape[1]  # Number of test assets
         self.k = factors.shape[1]  # Number of factors
 
         self.mu_y = self.y.mean()
@@ -62,8 +88,6 @@ class BFM:
         )
 
     def _draw_mu_sigma(self):
-        # TODO Documentation
-
         # Draws covariance from inverse wishart
         draws_sigma = invwishart.rvs(
             df=self.t - 1,
@@ -85,7 +109,6 @@ class BFM:
         return draws_mu, draws_sigma
 
     def _compute_betas(self):
-        # TODO Documentation
         draws_beta = np.array(
             [
                 cov[:self.n, -self.k:] @ inv(cov[-self.k:, -self.k:])
@@ -95,7 +118,6 @@ class BFM:
         return draws_beta
 
     def _compute_lambdas(self):
-        # TODO Documentation
         # campute lambdas from betas and mus
         draws_lambda = np.array(
             [
@@ -107,7 +129,6 @@ class BFM:
         return draws_lambda
 
     def _compute_r2(self):
-        # TODO Documentation
         mu_r = self.mu_y[:self.n].values
         denom = (mu_r - mu_r.mean()) @ (mu_r - mu_r.mean())
         draws_r2 = [
@@ -117,6 +138,15 @@ class BFM:
         return draws_r2
 
     def plot_lambda(self, include_fm=False):
+        """
+        Plots the posterior distributions for the risk premia
+
+        Parameters
+        ----------
+        include_fm : bool
+            If true, adds a vertical line indicating the values for the
+            canonical OLS Fama-MacBeth regression.
+        """
         axes = self.draws_lambdas.hist(
             density=True,
             bins=int(np.sqrt(self.n_draws)),
@@ -140,20 +170,28 @@ class BFM:
         plt.show()
 
     def plot_r2(self):
-        # TODO Documentation
+        """
+        Plots the posteurior distribution for the R2
+        """
         axes = self.draws_r2.hist(
             density=True,
             bins=int(np.sqrt(self.n_draws)),
             figsize=(10, 6)
         )
-
-        axes.axvline(0, color='tab:orange', lw=1)
-
         plt.tight_layout()
         plt.show()
 
     def ci_table_lambda(self, cred=0.95):
-        # TODO Documentation
+        """
+        Return a DataFrame with the median and the credible interval with the
+        chosen level of credibility for the risk premia
+
+        Parameters
+        ----------
+        cred : float
+            number between 0 and 1 indicating the credibility level of the
+            interval
+        """
         table = self.draws_lambdas.quantile(
             q=[
                 (1 - cred) / 2,
@@ -164,7 +202,16 @@ class BFM:
         return table
 
     def ci_table_r2(self, cred=0.95):
-        # TODO Documentation
+        """
+        Return a DataFrame with the median and the credible interval with the
+        chosen level of credibility for the R2
+
+        Parameters
+        ----------
+        cred : float
+            number between 0 and 1 indicating the credibility level of the
+            interval
+        """
         table = self.draws_r2.quantile(
             q=[
                 (1 - cred) / 2,
@@ -176,10 +223,12 @@ class BFM:
 
 
 class BFMGLS(BFM):
-    # TODO Documentation
+    """
+    This class inherits all the methods and constructor from BFM but modifies
+    the computation of the risk premia and R2 to use the GLS precision matrix
+    """
 
     def _compute_lambdas(self):
-        # TODO Documentation
         # compute idiosyncratic error covariance
         draws_sige = np.array(
             [
@@ -199,7 +248,6 @@ class BFMGLS(BFM):
         return draws_lambda
 
     def _compute_r2(self):
-        # TODO Documentation
         draws_sige = np.array(
             [
                 cov[:self.n, :self.n] - cov[:self.n, -self.k:] @ inv(cov[-self.k:, -self.k:]) @ cov[:self.n, -self.k:].T
@@ -218,14 +266,34 @@ class BFMGLS(BFM):
 
 
 class BFMOMIT(BFM):
-    # TODO Documentation
+    """
+    This class inherits all the methods and constructor from BFM but modifies
+    the computation of the risk premia and R2 to use the BFM-OMIT methodology
+    """
 
     def __init__(self, assets, factors, n_draws=1000, p=5):
+        """
+        Implementation of the BFM-OMIT
+
+        Parameters
+        ----------
+        assets : pandas.DataFrame
+            returns of the test assets
+
+        factors : pandas.DataFrame
+            returns of the factor portfolios
+
+        n_draws : int
+            number of draws from the posterior
+
+        p : int
+            number of principal components to use
+        """
         self.p = p
         super().__init__(assets, factors, n_draws)
 
     def _compute_lambdas(self):
-        # TODO Documentation
+
         def cov_svd(cov):
             u, s, _ = svd(cov)
             return (u @ np.diag(s))[:, :self.p]
@@ -254,7 +322,7 @@ class BFMOMIT(BFM):
         return draws_lambda_f
 
     def _compute_r2(self):
-        # TODO Documentation
+
         def cov_svd(cov):
             u, s, _ = svd(cov)
             return (u @ np.diag(s))[:, :self.p]
@@ -295,14 +363,14 @@ ports.columns = [f"FF{(s - 1) * 5 + v}" for s, v in ports.columns]
 facts = get_ff5f()
 
 tic = time()
-bfm = BFMOMIT(
+bfm = BFM(
     assets=ports,
     factors=facts,
-    n_draws=10000,
-    p=10,
+    n_draws=100000,
+    # p=10,
 )
 print(time() - tic)
 # print(bfm.ci_table_lambda())
 # print(bfm.ci_table_r2())
-bfm.plot_lambda(include_fm=True)
+bfm.plot_lambda(include_fm=False)
 # bfm.plot_r2()

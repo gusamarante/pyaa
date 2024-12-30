c9f795fab96956f3f923b49018527a8381cd9b7b
gamarante <gustavo.amarante@bwgi.com.br>
2024-12-30 12:45:29 -0300

    Single NTNB tracker chart

--- charts/measuring returns/ntnb_single_bond.py
+++ charts/measuring returns/ntnb_single_bond.py
@@ -2,9 +2,8 @@ import numpy as np
 from matplotlib.ticker import ScalarFormatter
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
-from tqdm import tqdm
-from time import time
-import pandas as pd
+from data import raw_ntnb
+from utils import AA_LECTURE, ASSET_ALLOCATION, BLUE
 
 last_year = 2024
 notional_start = 100
@@ -12,15 +11,7 @@ start_date = '2006-01-01'
 size = 5
 
 # Read the Data
-ntnb = pd.DataFrame()
-
-for year in tqdm(range(2003, last_year + 1), 'Reading files'):
-    aux = pd.read_csv(f'/Users/gustavoamarante/PycharmProjects/pyaa/trackers/input data/dados_ntnb {year}.csv', sep=';')
-    ntnb = pd.concat([ntnb, aux])
-
-ntnb['reference date'] = pd.to_datetime(ntnb['reference date'])
-ntnb['maturity'] = pd.to_datetime(ntnb['maturity'])
-ntnb = ntnb.drop(['Unnamed: 0', 'index'], axis=1)
+ntnb = raw_ntnb()
 
 ntnb = ntnb[ntnb['maturity'] == "2023-05-15"]  # Choose the bond here
 ntnb = ntnb[ntnb['du'] >= 0]
@@ -35,7 +26,7 @@ ntnb['notional'] = ntnb['quantity'] * ntnb['price']
 
 # Volatility
 ret = ntnb['notional'].pct_change(1).dropna()
-vol = ret.rolling(252).std() * np.sqrt(252)
+vol = ret.ewm(com=252, min_periods=126).std() * np.sqrt(252)
 
 
 # =================
@@ -43,18 +34,9 @@ vol = ret.rolling(252).std() * np.sqrt(252)
 # =================
 fig = plt.figure(figsize=(size * (16 / 7.3), size))
 
-ax = plt.subplot2grid((1, 2), (0, 0))
-ax.set_title("Total Return Index - NTNB 2023")
-ax.plot(ntnb['notional'], label="TRI", color="#3333B2", lw=2)
-ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
-ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5, which="both")
-ax.get_yaxis().set_major_formatter(ScalarFormatter())
-ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
-ax.tick_params(rotation=90, axis="x")
-
-ax = plt.subplot2grid((1, 2), (0, 1))
-ax.set_title("Annualized 1y Volatility - NTNB 2023")
-ax.plot(vol, label="Annualized Volatility", color="#3333B2", lw=2)
+ax = plt.subplot2grid((1, 1), (0, 0))
+ax.set_title("Total Return Index - NTNB Maturing in May/2023")
+ax.plot(ntnb['notional'], label="TRI", color=BLUE, lw=2)
 ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
 ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5, which="both")
 ax.get_yaxis().set_major_formatter(ScalarFormatter())
@@ -63,7 +45,8 @@ ax.tick_params(rotation=90, axis="x")
 
 plt.tight_layout()
 
-plt.savefig('/Users/gustavoamarante/Library/CloudStorage/Dropbox/Aulas/Asset Allocation/Figures/Measuring Returns - Single NTNB Total Return.pdf')
+plt.savefig(AA_LECTURE.joinpath('Figures/Measuring Returns - Single NTNB Total Return.pdf'))
+plt.savefig(ASSET_ALLOCATION.joinpath('Measuring Returns - Single NTNB Total Return.pdf'))
 plt.show()
 plt.close()
 
@@ -73,9 +56,18 @@ plt.close()
 # =================
 fig = plt.figure(figsize=(size * (16 / 7.3), size))
 
-ax = plt.subplot2grid((1, 1), (0, 0))
-ax.set_title("Daily Returns - NTNB 2023")
-ax.plot(ret, label="Returns", color="#3333B2", lw=2)
+ax = plt.subplot2grid((2, 1), (0, 0))
+ax.set_title("Daily Returns - NTNB Maturing in May/2023")
+ax.plot(ret, label="Returns", color=BLUE, lw=2)
+ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
+ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5, which="both")
+ax.get_yaxis().set_major_formatter(ScalarFormatter())
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+ax.tick_params(rotation=90, axis="x")
+
+ax = plt.subplot2grid((2, 1), (1, 0), sharex=ax)
+ax.set_title("Annualized 1y Volatility - NTNB Maturing in May/2023")
+ax.plot(vol, label="Annualized Volatility", color=BLUE, lw=2)
 ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
 ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5, which="both")
 ax.get_yaxis().set_major_formatter(ScalarFormatter())
@@ -84,6 +76,7 @@ ax.tick_params(rotation=90, axis="x")
 
 plt.tight_layout()
 
-plt.savefig('/Users/gustavoamarante/Library/CloudStorage/Dropbox/Aulas/Asset Allocation/Figures/Measuring Returns - Single NTNB Returns.pdf')
+plt.savefig(AA_LECTURE.joinpath('Figures/Measuring Returns - Single NTNB Returns and Vol.pdf'))
+plt.savefig(ASSET_ALLOCATION.joinpath('Measuring Returns - Single NTNB Returns and Vol.pdf'))
 plt.show()
 plt.close()

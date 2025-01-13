40f87d65ac8d1fca0b384a96e2aa6a327512433e
gamarante <gustavo.amarante@bwgi.com.br>
2025-01-13 17:17:03 -0300

    Progress on HMM, but not really

--- courses/asset allocation/hmm_regimes2.py
+++ courses/asset allocation/hmm_regimes2.py
@@ -5,25 +5,53 @@ import numpy as np
 from pomegranate.distributions import Normal
 from pomegranate.hmm import DenseHMM
 
+# User defined parameters
+n_states = 4
 
-np.random.seed(666)  # TODO test without the seed
-
-file_path = "/Users/gustavoamarante/Library/CloudStorage/Dropbox/NAVs.xlsx"
-
+# Read data
+file_path = r"C:\Users\gamarante\Dropbox\NAVs.xlsx"
 df = pd.read_excel(file_path, index_col=0)
 df.index = pd.to_datetime(df.index)
+rets = df.resample("M").last().pct_change(1).dropna()
 
-rets = df.resample("ME").last().pct_change(1).dropna()
-
+# The model
 hmm = DenseHMM(
-    distributions=[
-        Normal(),
-        Normal(),
-        Normal(),
-        Normal(),
-    ],
+    distributions=[Normal() for _ in range(n_states)],
+    verbose=True,
+)
+hmm = hmm.fit(X=np.array([rets.values]))
+
+trans_prob = pd.DataFrame(np.exp(np.array(hmm.edges)))
+trans_prob = trans_prob.div(trans_prob.sum(axis=1), axis=0)  # Reduce numerical error
+
+
+# Stationary distribution
+vals, vecs = np.linalg.eig(trans_prob)
+stat_dist = pd.Series(vecs[:, np.argmax(vals)])
+stat_dist = stat_dist * np.sign(stat_dist)
+stat_dist = stat_dist / stat_dist.sum()
+
+stat_dist.plot(kind="bar")
+plt.show()
+
+
+# States probabilities
+state_probs = pd.DataFrame(
+    data=hmm.predict_proba(np.array([rets.values]))[0],
+    index=rets.index,
 )
 
-hmm.fit(X=np.array([rets.values]))
+state_probs.plot()
+plt.show()
 
-a = 1
+
+# Predicted / Most likely State
+state_pred = pd.Series(
+    data=hmm.predict(np.array([rets.values]))[0],
+    index=rets.index,
+)
+state_pred.plot()
+plt.show()
+
+# TODO Normal Mixtures
+

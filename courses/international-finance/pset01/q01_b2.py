bb350e5c7c5a9d861bef5f2608342d4ca15d364a
gustavoamarante <gusamarante@gmail.com>
2025-02-24 10:30:30 -0300

    Progress on question 2

--- courses/international-finance/pset01/q01_b2.py
+++ courses/international-finance/pset01/q01_b2.py
@@ -27,15 +27,21 @@ data_wb = pd.read_excel(
 data_wb.index = pd.to_datetime(data_wb.index)
 data_wb = data_wb.resample("Y").last()
 
-series_dict = {
+
+def hp(series, lamb):
+    cycle, _ = hpfilter(series, lamb=lamb)
+    return cycle
+
+
+cycle_series = {
     "US": {
-        "GDP": "",
-        "Consumption": "",
-        "Investment": "",
-        "Government Spending": "",
-        "Exports": "",
-        "Imports": "",
-        "Trade Balance (% of GDP)": "",
+        "GDP": hp(np.log(data_wb['US GDP'].dropna()), 100),
+        "Consumption": hp(np.log(data_wb['US Cons'].dropna()), 100),
+        "Investment": hp(np.log(data_wb['US Inv'].dropna()), 100),
+        "Government Spending": hp(np.log(data_wb['US Gov'].dropna()), 100),
+        "Exports": hp(np.log(data_wb['US Exp'].dropna()), 100),
+        "Imports": hp(np.log(data_wb['US Imp'].dropna()), 100),
+        "Trade Balance (% of GDP)": ((data_wb['US Exp'] - data_wb['US Imp']) / data_wb['US Imp']).dropna(),  # TODO why is this not working?
     },
     "KR": {
         "GDP": "",
